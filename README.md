11bb7602cf06a2cd35c4771724937c7e9f44d44a
Cory Johns <johnsca@gmail.com>
2018-08-01 14:20:06 -0400

    Initial implementation

---
+++ README.md
@@ -0,0 +1,5 @@
+Overview
+========
+
+This is the charm layer for the kubeflow-pytorch-operator charm, which deploys
+PyTorch Training configured for use with Kubeflow to Kubernetes models in Juju.
---
+++ reactive/kubeflow_pytorch_operator.py
@@ -0,0 +1,65 @@
+import os
+import yaml
+
+from charmhelpers.core import hookenv
+from charms.reactive import set_flag
+from charms.reactive import when, when_not
+
+from charms import layer
+
+
+@when('layer.docker-resource.pytorch-operator-image.available')
+@when_not('charm.kubeflow-pytorch-operator.started')
+def start_charm():
+    layer.status.maintenance('configuring container')
+
+    config = hookenv.config()
+    conf_dir = '/etc/config'
+    conf_file = 'controller_config_file.yaml'
+    conf_path = '/'.join([conf_dir, conf_file])
+    image_info = layer.docker_resource.get_info('pytorch-operator-image')
+
+    conf_data = {}
+    if config['pytorch-default-image']:
+        conf_data['pytorchImage'] = config['pytorch-default-image']
+
+    layer.caas_base.pod_spec_set({
+        'containers': [
+            {
+                'name': 'pytorch-operator',
+                'imageDetails': {
+                    'imagePath': image_info.registry_path,
+                    'username': image_info.username,
+                    'password': image_info.password,
+                },
+                'command': [
+                    '/pytorch-operator',
+                    '--controller-config-file={}'.format(conf_path),
+                    '--alsologtostderr',
+                    '-v=1',
+                ],
+                'ports': [
+                    {
+                        'name': 'dummy',
+                        'containerPort': 9999,
+                    },
+                ],
+                'config': {
+                    'MY_POD_NAMESPACE': os.environ['JUJU_MODEL_NAME'],
+                    'MY_POD_NAME': hookenv.service_name(),
+                },
+                'files': [
+                    {
+                        'name': 'configs',
+                        'mountPath': conf_dir,
+                        'files': {
+                            conf_file: yaml.dump(conf_data),
+                        },
+                    },
+                ],
+            },
+        ],
+    })
+
+    layer.status.maintenance('creating container')
+    set_flag('charm.kubeflow-pytorch-operator.started')
